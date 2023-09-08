e884f5af6d12f54a8998b211b610dfc9c12e66f9
Ant46828 <rumbomongto@gmail.com>
2023-09-08 20:40:01 +0700

    C

---
+++ TTCN.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+printf ("Toi ten la Vu Nguyen Truong An \nTuoi:17");
+
+ 
+
+
+	return 0;
+}
