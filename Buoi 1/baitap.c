b40db4622749e9be029f6ee807191d472c3f617e
Ant46828 <rumbomongto@gmail.com>
2023-09-12 17:08:32 +0700

    C

---
+++ Buoi 1/baitap.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+printf("Toi ten la Vu Nguyen Truong An \nTuoi:17 \nDia chi: Tran Phu, Hoang Mai, Ha Noi \nEmail:ant86428@gmail.com \nSdt:0968241482");
+
+int a=1, b=2, c=3, d=4, e=5, f=6;
+int x, y; 
+x= a^6 + b*c*d + e*f;
+y= a^3*b^2 + a*b*d*e*f;
+printf ("\nx= %d ,\ny= %d", x, y ); 
+
+printf ("\n*"); 
+printf ("\n**");
+printf ("\n***"); 
+printf ("\n****"); 
+printf ("\n*****"); 
+
+printf ("\n========================="); 
+printf ("\n|STT|Ho Ten  |Tuoi |Gioi Tinh|"); 
+printf ("\n|01 |NguyenA |20   |Nam      |");
+printf ("\n|02 |NguyenB |22   |Nam      |");
+printf ("\n|03 |NguyenC |28   |Nam      |");
+
+d= 10*a*a + 5*b*c + c*c;
+printf ("\nd= %d", d);
+
+	return 0;
+}
